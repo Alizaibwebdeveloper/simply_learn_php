1795901566da5214dc26edf2ced30effac013a52
Ali zaib <alizaib12220@gmail.com>
2024-03-05 22:54:06 +0500

    FIRST commit of simplylearn php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simply php</title>
+</head>
+<body>
+    <h1>php Developer</h1>
+    <?php
+
+
+//  $str = "Hello! php developers!.";
+
+// echo  $str;
+
+
+// define("laptop","hp, lenovo and Toshiba");
+// echo "laptop";
+
+$var = 8;
+if($var>7){
+    echo " You fulfilll the given condition!";
+}else{
+    echo "plzz fulfill the condition first!";
+}
+
+    ?>
+</body>
+</html>
